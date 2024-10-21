39c754e7c36cf4da7f6f15dbe74e03f9452fe622
kaiti <122762348+kait-bit@users.noreply.github.com>
2024-10-21 12:40:34 -0500

    created a data struct that holds an id and information string for use in the graph.

---
+++ data.h
@@ -0,0 +1,17 @@
+/*********************
+Name: Kaitlin Spell
+Final Project
+Purpose: This program creates an unweighted and undirected graph.
+**********************/
+#ifndef GRAPH_DATA_H
+#define GRAPH_DATA_H
+
+#include <string>
+using std::string;
+
+struct Data {
+    int id;
+    string information;
+};
+
+#endif /* GRAPH_DATA_H */
